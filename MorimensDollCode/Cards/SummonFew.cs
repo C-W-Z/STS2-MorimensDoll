8ef466c8d1368222d4a04f540f8925be2b858684
C-W-Z <cwz.me@proton.me>
2026-06-06 22:11:20 +0800

    feat: update card SummonFew to lose hp instead of exhaust

--- MorimensDollCode/Cards/SummonFew.cs
+++ MorimensDollCode/Cards/SummonFew.cs
@@ -1,6 +1,8 @@
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 using MorimensDoll.Characters;
 using MorimensDoll.Minion;
 using STS2RitsuLib.Interop.AutoRegistration;
@@ -13,18 +15,19 @@ public sealed class SummonFew() : AbstractDollCard(2, CardType.Skill, CardRarity
 {
     protected override HashSet<CardTag> CanonicalTags => [];
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new RepeatVar(2)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new HpLossVar(4m), new RepeatVar(2)];
 
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust, CardKeyword.Retain];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
+        await CreatureCmd.Damage(choiceContext, Owner.Creature, DynamicVars.HpLoss.BaseValue, ValueProp.Unblockable, Owner.Creature, this);
         for (int i = 0; i < DynamicVars.Repeat.BaseValue; i++)
             await DollMinionCmd.Summon(choiceContext, Owner, this);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Repeat.UpgradeValueBy(1);
+        DynamicVars.HpLoss.UpgradeValueBy(-2);
     }
 }
